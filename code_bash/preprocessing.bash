edf8391de820aaee18172a37f1ce415a5988c7ab
dasaqui <dasaqui@gmail.com>
2022-06-26 01:08:24 -0500

    preprocessing: check path and extension

--- code_bash/preprocessing.bash
+++ code_bash/preprocessing.bash
@@ -13,6 +13,15 @@
 
 # Checking that there is only one argument and exit if not
 [[ $# -ne 1 ]] && $(
-    bash code_bash/error_flag.bash $0 null "script called with '$#' arguments"
+    bash code_bash/error_flag.bash "$0" null "script called with '$#' arguments"
     ) && exit 4
 
+# Checking the path and extension of the source file
+path="data\/00 data ingestion\/"
+extension="\(csv\|vcf\|vcf\.gz\)"
+pathern="${path}.*${extension}"
+[[ -n $( echo "$1"| sed "s/${pathern}//g") ]] && $(
+    bash code_bash/error_flag.bash "$0" "$1" "pathern or extension error:
+        expected path: '$( echo ${path} |sed s/\\\\//g)'
+        expected extension: $( echo ${extension} |sed s/\\\\//g) in lower case"
+    ) && exit 5
