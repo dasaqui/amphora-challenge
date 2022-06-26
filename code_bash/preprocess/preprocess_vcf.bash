4d889f3128b903d0d64c9361eca50ff2b456d4c2
dasaqui <dasaqui@gmail.com>
2022-06-26 17:38:51 -0500

    vcf process: arguments verification

--- code_bash/preprocess/preprocess_vcf.bash
+++ code_bash/preprocess/preprocess_vcf.bash
@@ -8,3 +8,7 @@
 #
 # The output vcf file will be sorted in dictionary order by chromosome
 # and position, and will be updated this change in the metadata
+
+# Checking that there is only one argument and exit if not
+[[ $# -ne 1 ]] && bash code_bash/error_flag.bash "$0" null "script called with '$#' arguments"
+[[ $# -ne 1 ]] && exit 7
