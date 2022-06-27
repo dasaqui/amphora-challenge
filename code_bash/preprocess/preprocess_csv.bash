fdd8409f697c6f2f64da07eacb14d2c3168c63e4
dasaqui <dasaqui@gmail.com>
2022-06-26 20:21:14 -0500

    csv headers and arguments

---
+++ code_bash/preprocess/preprocess_csv.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#####
+# This code is to preprocess one csv file
+#
+# This preprocessing code trust that the format is really a valid csv
+#
+# The output vcf file will be sorted in dictionary order by chromosome
+# and position, and will be logged this change in the metadata
+
+# Checking that there is only one argument and exit if not
+[[ $# -ne 1 ]] && bash code_bash/error_flag.bash "$0" null "script called with '$#' arguments"
+[[ $# -ne 1 ]] && bash "code_bash/error_flag.bash" "$0" "null" "Called without arguments"
+[[ $# -ne 1 ]] && exit 7
