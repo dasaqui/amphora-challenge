619f3c266ee8322cc590e4c2c8563244a56471b8
dasaqui <dasaqui@gmail.com>
2022-07-01 16:16:11 -0500

    pandas

--- code_python/libraries.py
+++ code_python/libraries.py
@@ -3,4 +3,11 @@ try:
     print( "sklearn is correctly installed\n")
 except:
     print( "python 'sklearn' module is not installed, please install it to continue")
-    print( "If you habe pip installed you can run:\npip install sklearn")
+    print( "If you habe pip installed you can run:\npip install sklearn\n")
+
+try:
+    import pandas
+    print( "pandas is correctly installed\n")
+except:
+    print( "python 'pandas' module is not installed, please install it to continue")
+    print( "If you habe pip installed you can run:\npip install pandas\n")
