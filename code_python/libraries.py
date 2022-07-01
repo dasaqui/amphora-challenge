2d91729404d6dfa17bf3e3d083d76b7b2263f18e
dasaqui <dasaqui@gmail.com>
2022-07-01 16:14:58 -0500

    sklearn

--- code_python/libraries.py
+++ code_python/libraries.py
@@ -1,5 +1,6 @@
 try:
     import sklearn
-    print( "sklearn is correctly installed")
+    print( "sklearn is correctly installed\n")
 except:
-    print ("python 'sklearn' module is not installed, please install it to continue")
+    print( "python 'sklearn' module is not installed, please install it to continue")
+    print( "If you habe pip installed you can run:\npip install sklearn")
