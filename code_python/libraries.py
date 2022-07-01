e50d4e500d135f9ffcb6df1f643339bcbe6a813c
dasaqui <dasaqui@gmail.com>
2022-07-01 16:44:45 -0500

    numba

--- code_python/libraries.py
+++ code_python/libraries.py
@@ -18,3 +18,10 @@ try:
 except:
     print( "python 'matplotlib' module is not installed, please install it to continue")
     print( "If you habe pip installed you can run:\npip install matplotlib\n")
+
+try:
+    import numba
+    print( "numba is correctly installed\n")
+except:
+    print( "python 'numba' module is not installed, please install it to continue")
+    print( "If you habe pip installed you can run:\npip install numba\n")
